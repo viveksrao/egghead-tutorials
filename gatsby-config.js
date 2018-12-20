8428f4e1c140dda7d8e12341ff2b8f5af18db0ad
Vivekanand Rao <vivekanand.rao@gmail.com>
2018-12-19 21:59:11 -0800

    adds netlify headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,8 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    'gatsby-transformer-remark'
+    'gatsby-transformer-remark',
+    'gatsby-plugin-netlify'
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
